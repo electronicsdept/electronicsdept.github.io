bb9137ea8f18e39a0a25527e6fd7c5764b30751c
Shreyan Nag <shreyannag@rocketmail.com>
2018-01-09 11:17:21 +0530

    Internet Explore 10 & 11 Supported Added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/*Navbar toggle*/
 function navbar(){
     var x = document.getElementById("myNav");
     if(x.className === "def"){
